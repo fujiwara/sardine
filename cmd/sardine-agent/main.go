1f5e9d2c19a43aade66b585fa5306ec112a354e6
FUJIWARA Shunichiro <fujiwara.shunichiro@gmail.com>
2017-12-04 17:25:29 +0900

    options reads from env

--- cmd/sardine-agent/main.go
+++ cmd/sardine-agent/main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"log"
 	"os"
+	"strings"
 
 	"github.com/fujiwara/sardine"
 )
@@ -12,10 +13,24 @@ func main() {
 	var config string
 	flag.StringVar(&config, "config", "", "config file path")
 	flag.BoolVar(&sardine.Debug, "debug", false, "enable debug logging")
+	flag.VisitAll(envToFlag)
 	flag.Parse()
 	err := sardine.Run(config)
 	if err != nil {
 		log.Println(err)
 		os.Exit(1)
+	}
+}
+
+func envToFlag(f *flag.Flag) {
+	names := []string{
+		strings.ToUpper(strings.Replace(f.Name, "-", "_", -1)),
+		strings.ToLower(strings.Replace(f.Name, "-", "_", -1)),
+	}
+	for _, name := range names {
+		if s := os.Getenv(name); s != "" {
+			f.Value.Set(s)
+			break
+		}
 	}
 }
